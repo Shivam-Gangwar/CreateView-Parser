887347d94206d319382d30154e1670d59b82b3c5
Shivam-Gangwar <82357581+Shivam-Gangwar@users.noreply.github.com>
2022-07-09 15:27:45 +0530

    Add files via upload

---
+++ project/y.tab.h
@@ -0,0 +1,126 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     crt = 258,
+     view = 259,
+     g = 260,
+     ob = 261,
+     id = 262,
+     cb = 263,
+     as = 264,
+     slct = 265,
+     comma = 266,
+     frm = 267,
+     whr = 268,
+     sc = 269,
+     star = 270,
+     nl = 271,
+     LT = 272,
+     GT = 273,
+     LE = 274,
+     GE = 275,
+     NE = 276,
+     AND = 277,
+     OR = 278,
+     EQ = 279,
+     si = 280,
+     di = 281,
+     ds = 282,
+     num = 283,
+     odr = 284,
+     by = 285,
+     asc = 286,
+     desc = 287,
+     nj = 288,
+     sum = 289,
+     cnt = 290,
+     avg = 291
+   };
+#endif
+/* Tokens.  */
+#define crt 258
+#define view 259
+#define g 260
+#define ob 261
+#define id 262
+#define cb 263
+#define as 264
+#define slct 265
+#define comma 266
+#define frm 267
+#define whr 268
+#define sc 269
+#define star 270
+#define nl 271
+#define LT 272
+#define GT 273
+#define LE 274
+#define GE 275
+#define NE 276
+#define AND 277
+#define OR 278
+#define EQ 279
+#define si 280
+#define di 281
+#define ds 282
+#define num 283
+#define odr 284
+#define by 285
+#define asc 286
+#define desc 287
+#define nj 288
+#define sum 289
+#define cnt 290
+#define avg 291
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
